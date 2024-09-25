dc6de5028cce5d4736ecc44e5d88f6b70300541b
Stefanie <multa.excogitare@gmail.com>
2024-09-25 09:30:18 +0200

    save status

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+# Created by Stefanie Sieg 2024/09
+
+# This page was inspired by the website (https://openlibrary.org/). It is a coding project and is not intended for commercial use.
+
+# The images used for the styling come from the websites (https://www.pinterest.de/) and (https://pixabay.com/). 
+
+# [Try out] (placeholder_for_link_after_deployment)
--- src/components/RandomList.jsx
+++ src/components/RandomList.jsx
@@ -1,88 +1,156 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const commonWords = ['the', 'be', 'to', 'of', 'and', 'in', 'that', 'have', 'it', 'for', 'not', 'on', 'with', 'he', 'she', 'as', 'you', 'do', 'at', 'this', 'but'];
+const commonWords = [
+  "the",
+  "be",
+  "to",
+  "of",
+  "and",
+  "in",
+  "that",
+  "have",
+  "it",
+  "for",
+  "not",
+  "on",
+  "with",
+  "he",
+  "she",
+  "as",
+  "you",
+  "do",
+  "at",
+  "this",
+  "but",
+];
 
 export default function RandomList() {
-    const [books, setBooks] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [selectedBook, setSelectedBook] = useState(null);
+  const [books, setBooks] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [selectedBook, setSelectedBook] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    function fetchBooks () {
-        setIsLoading(true);
-        const randomWord = commonWords[Math.floor(Math.random() * commonWords.length)];
-        const url = `https://openlibrary.org/search.json?q=${randomWord}&limit=20`;
+  function fetchBooks() {
+    setIsLoading(true);
+    const randomWord =
+      commonWords[Math.floor(Math.random() * commonWords.length)];
+    const url = `https://openlibrary.org/search.json?q=${randomWord}&limit=20`;
 
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                setBooks(data.docs);
-                setIsLoading(false);
-            })
-            .catch(() => {
-                setIsLoading(false);
-            });
-    };
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setBooks(data.docs);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setIsLoading(false);
+      });
+  }
 
-    useEffect(() => {
-        fetchBooks();
-    }, []);
+  useEffect(() => {
+    fetchBooks();
+  }, []);
 
-    function handleBookClick(book) {
-        setSelectedBook(book);
-    };
+  function handleBookClick(book) {
+    setSelectedBook(book);
+  }
 
-    function closeDetails() {
-        setSelectedBook(null);
-    };
+  function closeDetails() {
+    setSelectedBook(null);
+  }
 
-    if (isLoading) return <div>Loading...</div>;
+  function toggleModal() {
+    setIsModalOpen((prevState) => !prevState);
+  }
 
-    return (
-        <div className="random-list-wrapper">
-            <ul>
-                {books.map((book, index) => (
-                    <li key={index} onClick={() => handleBookClick(book)}>
-                        <img 
-                            src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`} 
-                            alt={book.title} 
-                        />
-                        <p>{book.title}</p>
-                    </li>
-                ))}
-            </ul>
+  if (isLoading) return <div>Loading...</div>;
 
-            {selectedBook && (
-                <div className="selected-book-wrapper">
-                    <h2>{selectedBook.title}</h2>
-                    <img 
-                        src={`https://covers.openlibrary.org/b/id/${selectedBook.cover_i}-L.jpg`} 
-                        alt={selectedBook.title} 
-                    />
-                    <p><strong>Author(s): </strong>{selectedBook.author_name ? selectedBook.author_name.join(", ") : "Unknown"}</p>
-                    <p><strong>First Published: </strong>{selectedBook.first_publish_year || "Unknown"}</p>
-                    <p><strong>ISBN: </strong>{selectedBook.isbn ? selectedBook.isbn[0] : "Unknown"}</p>
-                    <p><strong>Publisher: </strong>{selectedBook.publisher ? selectedBook.publisher[0] : "Unknown"}</p>
-                    <p><strong>Number of pages: </strong>{selectedBook.number_of_pages || "Unknown"}</p>
-                    <p><strong>Subjects: </strong>{selectedBook.subject ? selectedBook.subject.slice(0 ,5).join(", ") : "Unknown"}</p>
-                    <p><strong>Average Rating: </strong>{selectedBook.ratings_average.toFixed(1) || "-"} / 5</p>
-                    {
-                        selectedBook.ebook_access === "borrowable" && (
-                            <p><strong>Ebook available: </strong>Yes (Borrowable)</p>
-                        )
-                    }
-                    {
-                        selectedBook.ebook_count_i > 0 && (
-                            <p><strong>Ebook count: </strong>{selectedBook.ebook_count_i}</p>
-                        )
-                    }
-                    <button onClick={closeDetails}>Close</button>
-                </div>
-            )}
+  return (
+    <div className="random-list-wrapper">
+      <ul>
+        {books.map((book, index) => (
+          <li key={index} onClick={() => handleBookClick(book)}>
+            <img
+              src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`}
+              alt={book.title}
+            />
+            <p>{book.title}</p>
+          </li>
+        ))}
+      </ul>
 
-            <div className="new-random-list-wrapper">
-                <p>Nothing of interest? Let us suggest other books!</p>
-                <button onClick={fetchBooks}>New suggestions</button>
-            </div>
+      {selectedBook && (
+        <div className="selected-book-wrapper">
+          <h2>{selectedBook.title}</h2>
+          <img
+            src={`https://covers.openlibrary.org/b/id/${selectedBook.cover_i}-L.jpg`}
+            alt={selectedBook.title}
+            onClick={toggleModal}
+            style={{cursor: 'pointer'}}
+          />
+          <p>
+            <strong>Author(s): </strong>
+            {selectedBook.author_name
+              ? selectedBook.author_name.join(", ")
+              : "Unknown"}
+          </p>
+          <p>
+            <strong>First Published: </strong>
+            {selectedBook.first_publish_year || "Unknown"}
+          </p>
+          <p>
+            <strong>ISBN: </strong>
+            {selectedBook.isbn ? selectedBook.isbn[0] : "Unknown"}
+          </p>
+          <p>
+            <strong>Publisher: </strong>
+            {selectedBook.publisher ? selectedBook.publisher[0] : "Unknown"}
+          </p>
+          <p>
+            <strong>Number of pages: </strong>
+            {selectedBook.number_of_pages || "Unknown"}
+          </p>
+          <p>
+            <strong>Subjects: </strong>
+            {selectedBook.subject
+              ? selectedBook.subject.slice(0, 5).join(", ")
+              : "Unknown"}
+          </p>
+          <p>
+            <strong>Average Rating: </strong>
+            {selectedBook.ratings_average.toFixed(1) || "-"} / 5
+          </p>
+          {selectedBook.ebook_access === "borrowable" && (
+            <p>
+              <strong>Ebook available: </strong>Yes (Borrowable)
+            </p>
+          )}
+          {selectedBook.ebook_count_i > 0 && (
+            <p>
+              <strong>Ebook count: </strong>
+              {selectedBook.ebook_count_i}
+            </p>
+          )}
+          <button onClick={closeDetails}>X</button>
         </div>
-    );
+      )}
+
+      {isModalOpen && (
+        <div className="modal-overlay" onClick={toggleModal}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <img
+              src={`https://covers.openlibrary.org/b/id/${selectedBook.cover_i}-L.jpg`}
+              alt={selectedBook.title}
+            />
+            <button onClick={toggleModal}>X</button>
+          </div>
+        </div>
+      )}
+
+      <div className="new-random-list-wrapper">
+        <p>Nothing of interest? Let us suggest other books!</p>
+        <button onClick={fetchBooks}>New suggestions</button>
+      </div>
+    </div>
+  );
 }
